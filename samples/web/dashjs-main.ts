b5b73ad9d8468b4325cba4f30e4ca7124ee81509
stevenlyons <steven@wirevice.com>
2026-04-10 13:52:04 -0700

    Fix error when getting dash.js version

--- samples/web/dashjs-main.ts
+++ samples/web/dashjs-main.ts
@@ -1,9 +1,9 @@
-import dashjs, { MediaPlayer } from "dashjs";
+import { MediaPlayer } from "dashjs";
 import { PlinthDashjs, VERSION } from "@wirevice/plinth-dashjs";
 import { VERSION as JS_VERSION } from "@wirevice/plinth-js";
 import { setupDemo, showVersions } from "./demo-shell.js";
 
-showVersions({ "plinth-dashjs": VERSION, "plinth-js": JS_VERSION, "dash.js": dashjs.Version() });
+showVersions({ "plinth-dashjs": VERSION, "plinth-js": JS_VERSION, "dash.js": MediaPlayer().create().getVersion() });
 
 setupDemo(async (url, video, sessionFactory) => {
   const player = MediaPlayer().create();
